5f2e544aefeb6ea5adffeafd69177f44089323c7
Oluwatobi Okunuga <tobi@macbook-pro.home>
2023-06-28 21:56:34 -0400

    Test and code gray background for disabled button

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,14 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-test("button has correct initial color, and updates when clicked", () => {
+test("button has correct initial color", () => {
 	render(<App />);
+
 	// find an element with a role of button and text of 'Change to blue'
 	const colorButton = screen.getByRole("button", { name: "Change to blue" });
 
 	// expect the background color to be red
-	expect(colorButton).toHaveStyle({ "background-color": "red" });
+	expect(colorButton).toHaveStyle({ backgroundColor: "red" });
 
 	// click button
 	fireEvent.click(colorButton);
@@ -21,6 +22,7 @@ test("button has correct initial color, and updates when clicked", () => {
 
 test("initial conditions", () => {
 	render(<App />);
+
 	// check that the button starts out enabled
 	const colorButton = screen.getByRole("button", { name: "Change to blue" });
 	expect(colorButton).toBeEnabled();
@@ -33,11 +35,11 @@ test("initial conditions", () => {
 test("Checkbox disables button on first click and enables on second click", () => {
 	render(<App />);
 	const checkbox = screen.getByRole("checkbox", { name: "Disable button" });
-	const button = screen.getByRole("button", { name: "Change to blue" });
+	const colorButton = screen.getByRole("button", { name: "Change to blue" });
 
 	fireEvent.click(checkbox);
-	expect(button).toBeDisabled();
+	expect(colorButton).toBeDisabled();
 
 	fireEvent.click(checkbox);
-	expect(button).toBeEnabled();
+	expect(colorButton).toBeEnabled();
 });
